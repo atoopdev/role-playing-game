47f9c60af9f2d70082220fd53d76e1c741f35ca1
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2022-02-28 17:28:20 -0800

    update dice

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ const hero = {
     name : "Wizard",
     avatar : "images/wizard.png",
     health : 60,
-    diceRoll : 6
+    diceRoll : [3,1,4],
+    diceCount: 3
 }
 
 const monster = {
@@ -12,19 +13,25 @@ const monster = {
     name : "Orc",
     avatar : "images/orc.png",
     health : 10,
-    diceRoll : 4
+    diceRoll : [2],
+    diceCount: 1
 }
 
 
 function renderCharacter(data){
-    const {elementID, name, avatar, health, diceRoll} = data
+    const {elementID, name, avatar, health, diceRoll, diceCount} = data
+    let diceHTML = ""
+
+    for (let i=0; i<diceCount; i++){
+        diceHTML += `<div class="dice">${diceRoll[i]}</div>`
+    }
 
     document.getElementById(elementID).innerHTML = `
     <div class="character-card">
         <h4 class="name"> ${name} </h4>
         <img class="avatar" src="${avatar}"/>
         <p class="health">health: <b> ${health} </b></p>
-        <div class="dice-container"><div class="dice"> ${diceRoll} </div>
+        <div class="dice-container">${diceHTML}
         </div>
     </div>        `
 }
