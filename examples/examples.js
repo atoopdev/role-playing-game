56f924589acd6a2d79f6f9475b560e765c21190c
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2022-03-07 12:28:15 -0800

    .reduce() examples

--- examples/examples.js
+++ examples/examples.js
@@ -18,12 +18,24 @@ const endOfLevelBosses = new Array(10).fill('🍄').map(function(mushroom){
 console.log(endOfLevelBosses)
 document.getElementById("mushrooms").innerHTML = endOfLevelBosses
 
-// reduce() - sums contents of an array
+// reduce() - can use to sum contents of an array. does other things too
 const rainJanuaryByWeek = [ 10, 20, 0, 122 ]
 // total = running total
 // currentElement = element being worked on
 // starts with element1 = total element2 = current element
 const rainTotalJan = rainJanuaryByWeek.reduce(function(total, currentElement){
     // define what operation to use on array elements
+    console.log(`total: ${total} + currentElement: ${currentElement}`)
     return total + currentElement
 })
+
+console.log(`Total rain in Jan: ${rainTotalJan}`)
+
+// ------------------------------------------------------------
+
+const grades = [75, 83, 66, 43, 55, 99, 87, 16, 89, 64, 70, 80, 94, 77, 66, 73]
+
+const gradeTotals = grades.reduce(function(total, currentElement){
+    return total + currentElement
+})
+console.log(`Grade totals: ${gradeTotals/grades.length}`)
